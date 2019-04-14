1dcdb51fde97bf09b5f150ef0d9676f8e3d0a46d
Pascal Le Merrer <pascal.lemerrer@orange.com>
2019-04-14 11:18:28 +0200

    Add TEA

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,50 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class, type_)
+import Html.Events exposing (onInput)
 
 
-view =
+type alias Model =
+    String
+
+
+type Msg
+    = InputChanged String
+
+
+initialModel : String
+initialModel =
+    ""
+
+
+view :
+    Model
+    -> Html Msg -- 1
+view model =
     div [ class "container" ]
         [ h1 [ class "title" ] [ text "elm image search" ]
-        , input [ type_ "text", class "medium input" ] []
+        , input
+            [ type_ "text"
+            , class "medium input"
+            , onInput InputChanged
+            ]
+            []
         ]
 
 
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        InputChanged value ->
+            value
+
+
+main : Program () Model Msg
 main =
-    view
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
