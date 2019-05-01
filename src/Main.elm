48a24bd62a38b7663911bc69545ef5d1bd391b5e
Pascal Le Merrer <pascal.lemerrer@orange.com>
2019-05-01 10:30:00 +0200

    Add communication with server

--- src/Main.elm
+++ src/Main.elm
@@ -3,29 +3,46 @@ module Main exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class, type_)
-import Html.Events exposing (onInput)
-
-
-type alias Model =
-    String
+import Html.Events exposing (onInput, onSubmit)
+import Http
 
 
 type Msg
     = InputChanged String
+    | FormSubmitted
+    | ResponseReceived (Result Http.Error String)
 
 
-initialModel : String
-initialModel =
-    ""
+type alias Model =
+    { searchTerms : String
+    , response : String
+    }
 
 
-view :
-    Model
-    -> Html Msg -- 1
+init :
+    ()
+    -> ( Model, Cmd Msg )
+init _ =
+    ( { searchTerms = ""
+      , response = ""
+      }
+    , Cmd.none
+    )
+
+
+view : Model -> Html Msg
 view model =
     div [ class "container" ]
         [ h1 [ class "title" ] [ text "elm image search" ]
-        , input
+        , viewForm
+        , viewResponse model
+        ]
+
+
+viewForm : Html Msg
+viewForm =
+    form [ onSubmit FormSubmitted ]
+        [ input
             [ type_ "text"
             , class "medium input"
             , onInput InputChanged
@@ -34,17 +51,47 @@ view model =
         ]
 
 
-update : Msg -> Model -> Model
+viewResponse : Model -> Html Msg
+viewResponse model =
+    div [] [ text model.response ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         InputChanged value ->
-            value
+            ( { model | searchTerms = value }, Cmd.none )
+
+        FormSubmitted ->
+            let
+                httpCommand =
+                    Http.get
+                        { url =
+                            "https://unsplash.noprod-b.kmt.orange.com"
+                                ++ "/search/photos?query="
+                                ++ model.searchTerms
+                        , expect = Http.expectString ResponseReceived
+                        }
+            in
+            ( model, httpCommand )
+
+        ResponseReceived (Ok jsonString) ->
+            ( { model | response = jsonString }, Cmd.none )
+
+        ResponseReceived (Err _) ->
+            ( { model | response = "Communication error" }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = init
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
